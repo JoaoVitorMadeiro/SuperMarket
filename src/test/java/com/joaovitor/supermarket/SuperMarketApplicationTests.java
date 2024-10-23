6373ef4a74f3a4473be42dc401cac9585f612149
João Vitor de Sousa Madeiro <143713858+JoaoVitorMadeiro@users.noreply.github.com>
2024-10-23 14:46:00 -0300

    Initial commit

---
+++ src/test/java/com/joaovitor/supermarket/SuperMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovitor.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
